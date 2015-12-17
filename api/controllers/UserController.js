b36615f680c5b2501486bcc8cf0ae5e88cc133e7
Arunabh Das <arunabhdas@gmail.com>
2015-12-17 06:23:01 -0500

    Added flash policy and changed UserController to original state

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,23 +7,15 @@
 
 module.exports = {
 	'new': function(req, res) {
-		res.locals.flash = _.clone(req.session.flash);
 		res.view();
-		req.session.flash = {};
 	},
 	create: function(req, res, next) {
 		User.create(req.params.all(), function userCreated(err, user) {
 			if (err) {
 				console.log(err);
-				req.session.flash = {
-						err: err
-				}
-				res.locals.flash = req.session.flash
 				return res.redirect('/user/new');
 			}
-			// Redirect to the show action
 			res.json(user);
-			req.session.flash = {};
 		})
 	}
 };
